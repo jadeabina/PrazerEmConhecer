269c4e5ad0a7cc7c90dbc31a4892f26023954659
Perkles <jade.aabinajm@gmail.com>
2021-08-29 00:13:04 -0300

    Initial commit

---
+++ src/primeiroPrograma/PrazerEmConhecer.java
@@ -0,0 +1,16 @@
+package primeiroPrograma;
+
+import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane;
+public class PrazerEmConhecer {
+
+    public static void main(String[] args) {
+        String resposta;
+        resposta=JOptionPane.showInputDialog("Qaul é o seu nome ?");
+
+        JOptionPane.showMessageDialog(null, " olá "  + resposta + " !é um prazer em conhece-lo(a)!");
+
+    }
+
+}
