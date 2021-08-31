665458607daabc1ba1e83890e42bad20f9d109b5
Perkles <jade.aabinajm@gmail.com>
2021-08-31 10:14:37 -0300

    atualização/correção

--- src/primeiroPrograma/PrazerEmConhecer.java
+++ src/primeiroPrograma/PrazerEmConhecer.java
@@ -2,12 +2,12 @@ package primeiroPrograma;
 
 import javax.swing.JOptionPane;
 
-import javax.swing.JOptionPane;
 public class PrazerEmConhecer {
 
+    //sistema que pergunta qual é seu nome e recebe com seja bem vindo
     public static void main(String[] args) {
         String resposta;
-        resposta=JOptionPane.showInputDialog("Qaul é o seu nome ?");
+        resposta=JOptionPane.showInputDialog("Qual é o seu nome ?");
 
         JOptionPane.showMessageDialog(null, " olá "  + resposta + " !é um prazer em conhece-lo(a)!");
 
